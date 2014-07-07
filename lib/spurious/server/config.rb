dc8c2b7ac728bd8c1b178b5239e1a18c2d647a36
Steven Jack <stevenmajack@gmail.com>
2014-07-07 15:14:48 +0100

    Adds image exists method to config

--- lib/spurious/server/config.rb
+++ lib/spurious/server/config.rb
@@ -9,6 +9,12 @@ module Spurious
         @app = YAML::load_file(config_location)
       end
 
+      def image_exists?(image_id)
+        app.any? do |type, meta|
+          meta[:image] == image_id
+        end
+      end
+
     end
   end
 end
--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -1,30 +1,36 @@
 require "helper/spec"
 
 describe Spurious::Server::Config do
+  let(:expected_hash) {
+    {
+      :image => 'foo/bar',
+      :name => 'foo-bar',
+      :link => [
+        'foo:bar'
+      ],
+      :env => [
+        { :FOO => 'bar' }
+      ]
+    }
+  }
+  let (:config_location) { File.join(File.dirname(__FILE__),'fixtures', 'config.yaml') }
+  let (:config) { Spurious::Server::Config.new(config_location) }
 
   describe ".app" do
-    let (:config_location) { File.join(File.dirname(__FILE__),'fixtures', 'config.yaml') }
-    let (:config) { Spurious::Server::Config.new(config_location) }
-
     it "Loads the config from a yaml file" do
       expect(config.app).to be_a(Hash)
     end
 
     it "Has the data needed to be a container" do
-      expected_hash = {
-        :image => 'foo/bar',
-        :name => 'foo-bar',
-        :link => [
-          'foo:bar'
-        ],
-        :env => [
-          { :FOO => 'bar' }
-        ]
-      }
-
       expect(config.app[:test]).to eq(expected_hash)
     end
 
+  end
+
+  describe ".image_exists(image_id)" do
+    it "Indicates that an image exists in the config" do
+      expect(config.image_exists? expected_hash[:image]).to be_truthy
+    end
   end
 
 end
