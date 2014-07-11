bed0b28db44b45738d16aded83f0552c21104401
Steven Jack <stevenmajack@gmail.com>
2014-07-11 09:25:23 +0100

    Uses tick method on event machine

--- lib/spurious/server/state/start.rb
+++ lib/spurious/server/state/start.rb
@@ -24,11 +24,12 @@ module Spurious
               container.start meta
 
               if container.json["Name"] == '/spurious-sqs' then
-                Thread.new do
-                  sleep 5
+                port_setup = Proc.new do
                   port = container.json["NetworkSettings"]["Ports"]['4568/tcp'].first['HostPort']
                   Net::HTTP.get(URI("http://#{docker_host_ip}:#{port}/host-details?host=#{docker_host_ip}&port=#{port}"))
                 end
+
+                EM.add_timer(5) { EM.defer(port_setup) }
               end
             rescue Exception => e
               error(e.message)
