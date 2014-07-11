97c998249ffa7541696780c63ec2d0891f14c9cf
Steven Jack <stevenmajack@gmail.com>
2014-07-11 09:15:26 +0100

    Pass docker_host through

--- lib/spurious/server.rb
+++ lib/spurious/server.rb
@@ -5,9 +5,9 @@ require "spurious/server/app"
 module Spurious
   module Server
 
-    def self.handle(ip, port)
+    def self.handle(ip, port, docker_host)
       Proc.new do
-        EventMachine.start_server ip, port, Spurious::Server::App
+        EventMachine.start_server ip, port, Spurious::Server::App, docker_host
       end
     end
 
