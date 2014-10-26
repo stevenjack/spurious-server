0c90456a104f332f83623ad3b5a5d78b9c7d6466
Steven Jack <stevenmajack@gmail.com>
2014-10-26 08:44:32 +0000

    Allows server to pick up DOCKER_CERT_PATH variable

--- lib/spurious/server/options.rb
+++ lib/spurious/server/options.rb
@@ -1,7 +1,7 @@
 module Spurious
   module Server
     class Options
-      attr_reader :docker_full_path, :docker_host, :docker_port, :docker_api, :server_port, :server_ip, :write_timeout, :read_timeout
+      attr_reader :docker_full_path, :docker_host, :docker_port, :docker_api, :server_port, :server_ip, :write_timeout, :read_timeout, :cert_path
 
       def initialize(env)
         @docker_host      = env['DOCKER_HOST'].nil? ? 'localhost' : env['DOCKER_HOST'][/\/\/([0-9a-z\.]+):/,1]
@@ -11,8 +11,27 @@ module Spurious
         @server_ip        = env.fetch('SPURIOUS_SERVER_IP', '0.0.0.0')
         @write_timeout    = env.fetch('EXCON_WRITE_TIMEOUT', 30000)
         @read_timeout     = env.fetch('EXCON_READ_TIMEOUT', 30000)
+        @cert_path        = env.fetch('DOCKER_CERT_PATH', nil)
+        setup_ssl if @cert_path
       end
 
+      def ssl_docker_host
+        "https://#{docker_host}:#{docker_port}"
+      end
+
+      protected
+
+      def setup_ssl
+        Docker.options = {
+          :client_cert => File.join(absolute_cert_path, 'cert.pem'),
+          :client_key  => File.join(absolute_cert_path, 'key.pem'),
+          :ssl_ca_file => File.join(absolute_cert_path, 'ca.pem')
+        }
+      end
+
+      def absolute_cert_path
+        @absolute_cert_path ||= File.expand_path cert_path
+      end
     end
   end
 end
